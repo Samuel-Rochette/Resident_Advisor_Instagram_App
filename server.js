4fe491722f37175a28a58f649bc855f5e5774e05
Tec Tale <samuelrmeyer@gmail.com>
2019-01-30 17:20:46 -0500

    /*

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + "/dist"));
 
 app.listen(process.env.PORT || 8080);
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 

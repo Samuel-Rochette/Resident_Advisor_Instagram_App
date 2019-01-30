6d4a3fb13f36234f7ddf52aab3ec17c0aaff8de6
Tec Tale <samuelrmeyer@gmail.com>
2019-01-30 17:26:32 -0500

    function

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(express.static(__dirname + "/dist"));
 
 app.listen(process.env.PORT || 8080);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 console.log("Console is listening");
